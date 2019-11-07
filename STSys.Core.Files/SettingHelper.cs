333ba178d8981f30f82444fb3f5a55a573ace8c5
git <https://gitee.com/xxtk/NASys.git>
2019-11-07 17:45:58 +0800

    1111111

---
+++ STSys.Core.Files/SettingHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STSys.Core.Files
+{
+    public class SettingHelper
+    {
+        static SettingHelper()
+        {
+            //加载配置文件
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(ProcessDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            //加载Ioc
+            serviceProvider = new ServiceCollection()
+                .AddSingleton<IConfiguration>(Configuration)
+  
+                .BuildServiceProvider();
+        }
+
+        public static string ProcessDirectory
+        {
+            get
+            {
+#if NETSTANDARD1_3
+                return AppContext.BaseDirectory;
+#else
+                return AppDomain.CurrentDomain.BaseDirectory;
+#endif
+            }
+        }
+
+        public static IConfigurationRoot Configuration { get; }
+        public static ServiceProvider serviceProvider { get; }
+    }
+}
